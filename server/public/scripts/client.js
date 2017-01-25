d706a5fb38deba46eac5624415b13780d03f2b5a
Brent Reno <brent.reno@gmail.com>
2017-01-25 16:40:16 -0600

    refactor to use jquery

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,38 +1,20 @@
-// test for handshake
-console.log("Hello from the Client Script!");
+$(document).ready(function(){
+  console.log("ready");
+  console.log("ready");
 
-//create angular app
-var myApp = angular.module("myApp", ["ngRoute"]);
-
-//config NG routes
-myApp.config(["$routeProvider", function($routeProvider){
-  $routeProvider.
-  //home
-  when("/home", {
-    templateUrl:'/views/partials/home.html',
-    controller:'homeController'
-  }).
-  //about
-  when("/about", {
-    templateUrl:'/views/partials/about.html',
-    controller:'aboutController'
-  }).
-  //photo
-  when("/photo", {
-    templateUrl:'/views/partials/photo.html',
-    controller:'photoController'
-  }).
-  //video
-  when("/video",{
-    templateUrl:'/views/partials/video.html',
-    controller: 'videoController'
-  }).
-  //web
-  when("/web",{
-    templateUrl:'/views/partials/web.html',
-    controller: 'webController'
-  }).
-  otherwise({
-    redirectTo:"/home"
+  $(".showSection").on("click", function(){
+    if($("#about").hasClass("show")){
+        $("#about").removeClass("show");
+    }
+    if($("#photo").hasClass("show")){
+        $("#photo").removeClass("show");
+    }
+    if($("#video").hasClass("show")){
+        $("#video").removeClass("show");
+    }
+    if($("#web").hasClass("show")){
+        $("#web").removeClass("show");
+    }
+    $(this).addClass(" show");
   });
-}]);//end config routes
+});
