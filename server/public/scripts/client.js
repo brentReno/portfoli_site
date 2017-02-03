deb65d603bf3da97cbc7a697569d101ae17d7d54
Brent Reno <brent.reno@gmail.com>
2017-02-03 13:37:14 -0600

    about section transition

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   console.log("ready");
+  // landing page to about click
+  $("#landingClick").on('click', function(){
+    console.log("Click works");
+    $("#about").css("left", 0);
+  });
 });
