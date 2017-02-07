82f9153fa0ddc7340bde15ee1dfe16a9d7bb4394
Brent Reno <brent.reno@gmail.com>
2017-02-07 11:39:16 -0600

    nav icon rotation

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
   console.log("ready");
+
   // landing page to about click
   $("#landingClick").on('click', function(){
     console.log("Click works");
     $("#about").css("left", 0);
   });// end landing click
+
   // expand nav click
   $("#menuUpDown").on('click', function(){
-    $("menuUpDown, img").toggle();
+    var navDispaly = $("#expandedNav").css("display");
+    if(navDispaly == "none"){
+      $("#navIcon").css("transform", "rotate(180deg)");
+    }
+    if(navDispaly =="block"){
+      $("#navIcon").css("transform", "rotate(0deg)");
+    }
     $("#expandedNav").toggle();
   });//end expandedNav
 });
