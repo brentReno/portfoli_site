899cfdfc6e4960c804996533ae061b2c2e44bcc7
Brent Reno <brent.reno@gmail.com>
2017-02-07 13:51:05 -0600

    reset nav on link click

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,47 +8,55 @@ $(document).ready(function(){
 
   // expand nav click
   $("#menuUpDown").on('click', function(){
-    var navDispaly = $("#expandedNav").css("display");
-    if(navDispaly == "none"){
-      $("#navIcon").css("transform", "rotate(180deg)");
-    }
-    if(navDispaly =="block"){
-      $("#navIcon").css("transform", "rotate(0deg)");
-    }
-    $("#expandedNav").toggle();
+    navToggle();
   });//end expandedNav
 
   //about nav click
   $("#aboutNav").on('click', function(){
     resetPages();
     $("#about").css("left", 0);
+    navToggle();
 
   });
   $("#photoNav").on('click',function(){
     resetPages();
     $("#photo").css("left", 0);
+    navToggle();
   });
   $("#videoNav").on('click', function(){
     resetPages();
     $("#video").css("top", 0);
+    navToggle();
     });
   $("#webNav").on('click', function(){
     resetPages();
     $("#web").css("top", 0);
+    navToggle();
     });
 
-    var resetPages = function (){
-      if($("#about").css("left",0)){
-        $("#about").css("left", "-1275px");
+  var resetPages = function (){
+    if($("#about").css("left",0)){
+      $("#about").css("left", "-1275px");
+    }
+    if($("#photo").css("left",0)){
+      $('#photo').css("left", "1275px");
+    }
+    if($("#video").css("left",0)){
+      $('#video').css("top", "-699px");
+    }
+    if($("#web").css("left",0)){
+      $('#web').css("top","699px");
+    }
+  };
+  var navToggle = function(){
+      var navDispaly = $("#expandedNav").css("display");
+      if(navDispaly == "none"){
+        $("#navIcon").css("transform", "rotate(180deg)");
       }
-      if($("#photo").css("left",0)){
-        $('#photo').css("left", "1275px");
+      if(navDispaly =="block"){
+        $("#navIcon").css("transform", "rotate(0deg)");
       }
-      if($("#video").css("left",0)){
-        $('#video').css("top", "-699px");
-      }
-      if($("#web").css("left",0)){
-        $('#web').css("top","699px");
-      }
-    };
+      $("#expandedNav").toggle();
+  };
+
 });
