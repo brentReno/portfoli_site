387a0e27311f8598ad9cbf69011ef64ff766837b
Brent Reno <brent.reno@gmail.com>
2017-02-11 16:17:27 -0600

    masonry working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   console.log("ready");
+  var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 80
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
+
 
   // landing page to about click
   $("#landingClick").on('click', function(){
